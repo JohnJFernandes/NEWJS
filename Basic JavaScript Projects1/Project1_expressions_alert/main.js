ace138e598c1bdbd42c21be6383957aa4aa29081
Zach Fernandez <121633815+JohnJFernandes@users.noreply.github.com>
2023-02-20 18:28:15 -0800

    push changes
    commit one more

--- Basic JavaScript Projects1/Project1_expressions_alert/main.js
+++ Basic JavaScript Projects1/Project1_expressions_alert/main.js
@@ -1,15 +1,11 @@
 
-document.getElementById(apples);
-document.getElementById(oranges);
-
+document.getElementById("apples");
+document.write(12 * 22);
 function myFunction ()  {                     //button display when clicked//
     var sentence = "I am learning, slowly\n";
     sentence += "a little from this course!";
     document.getElementById("Concatenate").innerHTML =sentence;
 }
+window.alert("One a day keeps the Dr. away");
 
-function myFunction(apples)  {
-    return 12 * 22;
-}
-document.getElementById(fruit)
 
