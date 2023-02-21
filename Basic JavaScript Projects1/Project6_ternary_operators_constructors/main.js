e913f5e875b7cdce283bf5e644fb62b8c4a09c12
Zach Fernandez <121633815+JohnJFernandes@users.noreply.github.com>
2023-02-20 21:45:09 -0800

    project6
    I think it is completed

---
+++ Basic JavaScript Projects1/Project6_ternary_operators_constructors/main.js
@@ -0,0 +1,38 @@
+function Nested_Function() {
+    document.getElementById("Counting").innerHTML = Count();
+    function Count () {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
+function Ride_Function()  {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_Ride = (Height < 52) ? "You are to short" :"You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_Ride + " to ride.";
+}
+
+function Vote_Function()  {
+    var Age, Can_Vote;
+    Age = document.getElementById("Age").value;
+    Can_Vote = (Age > 18) ? "You can" :"You are not old enough to";
+    document.getElementById("Vote").innerHTML = Can_Vote + " Vote.";
+}
+
+function Vehicle(Make, Model, year, Color)  {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trailhawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction()  {
+    document.getElementById("Keywords_and_Constructors").innerHTML =
+    "Erik drives a " +Erik.Vehicle_Color + "-colored" + Erik.Vehicle_Model +
+    "manufactured in" +Erik.Vehicle_Year;
+}
+
