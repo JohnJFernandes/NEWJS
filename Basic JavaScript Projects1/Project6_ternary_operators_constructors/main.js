4e973978a61919d5a65e6a22162b1c17c5ff9532
Zach Fernandez <121633815+JohnJFernandes@users.noreply.github.com>
2023-02-22 19:37:15 -0800

    final touches
    ran in browser, good

--- Basic JavaScript Projects1/Project6_ternary_operators_constructors/main.js
+++ Basic JavaScript Projects1/Project6_ternary_operators_constructors/main.js
@@ -31,7 +31,7 @@ var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
 var Emily = new Vehicle("Jeep", "Trailhawk", 2019, "White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 function myFunction()  {
-    document.getElementById("Keywords_and_Constructors").innerHTML =
+    document.getElementById("New_and_This").innerHTML =
     "Erik drives a " +Erik.Vehicle_Color + "-colored" + Erik.Vehicle_Model +
     "manufactured in" +Erik.Vehicle_Year;
 }
