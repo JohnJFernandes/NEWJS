10dea607d17205c75b73888fa6ca9425de19def2
Zach Fernandez <121633815+JohnJFernandes@users.noreply.github.com>
2023-02-28 18:40:02 -0800

    project10
    a start, need help finishing

---
+++ Basic JavaScript Projects1/Project10_loops_arrays/main.js
@@ -0,0 +1,56 @@
+
+function count_From_Ten()  {
+    var Digit = "";
+    var X = 10;
+    while (X>0)  {
+        Digit -= "<br>" + X;
+        X++;
+    }
+    document.getElementById()
+}
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop()  {
+    for (Y = 0; Y < Instruments.length; Y++)  {
+    Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML =Content;
+}
+
+function array_Function()  {
+    var Dog_Picture = [];
+    Dog_Picture[0] ="";
+    Dog_Picture[1] ="";
+    Dog_Picture[2] ="";
+    Dog_Picture[3] ="";
+    document.getElementById("Array").innerHTML ="in this picture, the cat is " + Dog_Picture[2] + ".";
+}
+let text = "One in a million";
+let length = text.length;
+document.getElementById().innerHTML = length;
+
+function constant_function()  {
+    firstName: "UserFirstName",
+    lastName: "UserLastName"
+};
+
+var x = 50;
+document.write(X);
+{
+    let X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+
+function myFunction(name)  {
+    return "Hello " + name;
+}
+
+let text1="Animal";
+for (let i = 0; i < 9; i++)  {
+    if (i === 3) { continue; }
+    text += "The number is " + i + "<br>";
+}
+document.getElementById(keep).innerHTML = text;
